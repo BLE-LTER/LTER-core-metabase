11df071c8d67ac6a032e4e82d5eea7ea8b1bd478
anthainguyen38 <an.thai.nguyen.38@gmail.com>
2019-04-17 15:17:46 -0500

    add VIEW and fix constraints
    tested minimally on An's PC

--- sql/07_create_missingvalues_tables.sql
+++ sql/07_create_missingvalues_tables.sql
@@ -1,19 +1,21 @@
 -- Note: FIRST: edit %db_owner% to the account name of the DB owner 
 
 CREATE TABLE lter_metabase."EMLMissingCodes" (
-	"CodeID" character varying(20) NOT NULL;
-	"MissingValueCode" character varying(200) NOT NULL;
-	"MissingValueCodeExplanation" character varying(1024) NOT NULL;
+	"CodeID" character varying(20) NOT NULL,
+	"MissingValueCode" character varying(200) NOT NULL,
+	"MissingValueCodeExplanation" character varying(1024) NOT NULL
 );
-
+ALTER TABLE ONLY lter_metabase."EMLMissingCodes" 
+	ADD CONSTRAINT "UQ_EMLMissingCodes_CodeID" UNIQUE("CodeID");
+	
 ALTER TABLE lter_metabase."EMLMissingCodes"
     OWNER TO %db_owner%;
 
 CREATE TABLE lter_metabase."DataSetMissingCodes" (
-	"DataSetID" integer NOT NULL;
-	"EntitySortOrder" integer NOT NULL;
-	"ColumnName" character varying(200) NOT NULL;
-	"MissingValueCode" character varying(200) NOT NULL;
+	"DataSetID" integer NOT NULL,
+	"EntitySortOrder" integer NOT NULL,
+	"ColumnName" character varying(200) NOT NULL,
+	"CodeID" character varying(200) NOT NULL
 );
 
 ALTER TABLE lter_metabase."DataSetMissingCodes"
@@ -21,7 +23,13 @@ ALTER TABLE lter_metabase."DataSetMissingCodes"
 	
 ALTER TABLE ONLY lter_metabase."DataSetMissingCodes"
 	ADD CONSTRAINT
-	"FK_DatasetMissingCode_CodeID" FOREIGN KEY ("CodeID") REFERENCES lter_metabase."EMLMissingCodes"("MissingValueCode") ON UPDATE CASCADE;
+	"FK_DatasetMissingCode_CodeID" FOREIGN KEY ("CodeID") REFERENCES lter_metabase."EMLMissingCodes"("CodeID") ON UPDATE CASCADE;
 	
 ALTER TABLE ONLY lter_metabase."DataSetMissingCodes"
-    ADD CONSTRAINT "PK_DataSetMissingCodes" PRIMARY KEY ("DataSetID", "EntitySortOrder", "ColumnName");
+    ADD CONSTRAINT "PK_DataSetMissingCodes" PRIMARY KEY ("DataSetID", "EntitySortOrder", "ColumnName", "CodeID");
+	
+CREATE VIEW mb2eml_r.vw_eml_missingcodes AS
+SELECT d."DataSetID" as datasetid, d."EntitySortOrder" as entity_position, d."ColumnName" as "attributeName", e."MissingValueCode" as "MissingValueCode", e."MissingValueCodeExplanation" as "MissingValueCodeExplanation" FROM (lter_metabase."DataSetMissingCodes" d JOIN lter_metabase."EMLMissingCodes" e ON (((d."CodeID")::text = (e."CodeID")::text))) ORDER BY d."DataSetID";
+
+ALTER TABLE mb2eml_r.vw_eml_missingcodes OWNER TO %db_owner%;
+
